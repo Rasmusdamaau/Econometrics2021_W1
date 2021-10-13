e8a2067d5de8645e42cfb2741f38b70cf523ffb7
Rasmusdamaau <Rasmus72dam@gmail.com>
2021-10-13 15:51:58 +0200

    mangler sidste opgave

--- self-study1-laden-vm2022-main/self_study_1.R
+++ self-study1-laden-vm2022-main/self_study_1.R
@@ -1,18 +1,30 @@
 library(data.table)
-
+library(magrittr)
 
 data <- rio::import("self-study1-laden-vm2022-main/m_wage.csv", setclass = "data.table")
 
 
 
-data[,
-     `:=`(mean_pre = mean(wage_st,na.rm=T), mean_post = mean(wage_st2,na.rm=T)),
-     by = nj
-][]
+DID <- function(data,pre,post) {
+  
+  means <- data[,
+                .(mean_pre = mean(get(pre),na.rm=T), mean_post = mean(get(post),na.rm=T)),
+                by = nj
+  ][]
+  # means
+  DID <- means[,.(mean_diff = mean_post - mean_pre), by = nj]
+  DID
+  
+}
 
-means <- data[,
-     .(mean_pre = mean(wage_st,na.rm=T), mean_post = mean(wage_st2,na.rm=T)),
-     by = nj
-][]
+DID(data,"wage_st","wage_st2")
 
-DID <- 
+DID(data,"emptot","emptot2")
+
+DID(data,"pmeal","pmeal2")
+
+
+long_d <- melt(data,id.vars = c("bk","kfc","wendys"),
+     measure.vars = c("wage_st","wage_st2","emptot","emptot2","pmeal","pmeal2"))
+
+long_d[,post := fifelse(grepl("2",variable),1,0)]
